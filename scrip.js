c6731cabbdffc5fce97329f0dee50da2d5c8d073
vitaodudycz <vitor.dudycz@escola.pr.gov.br>
2025-09-03 20:14:11 -0300

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,39 @@
+//tipo de dados
+//tipo STRING: TEXTO, IDENTIFICADO PELO USO DE ASPAS
+//DUPLAS
+let nome = " Vitor";
+
+// tipo NUMBER: se trata de número
+
+let num = 10 
+let num2 = "10";
+
+//tipo booleano: aceita dois valores como referencia e retorna um dado com base na condição.
+//um dado com base na condição.
+
+//let ave = "pinguin";
+//if (ave = true){
+ //   alert("é uma ave.")
+//} else {
+   // alert("não é ave");
+//}
+
+alert( "ola seja bem vindo ao meu site");
+alert("precisamos confirmar alguns dados, antes de começar");
+let = seuNome = prompt(" digite seu nome por favor");
+
+
+alert("seja bem vindo"  + seuNome + " ao meu site" ); //usamos concatenação
+alert( " agora prescisamos saber a sua idade");
+let idade = prompt("por favor digite sua idade");
+alert("ja sei que você tem" + idade + " e agora preciso deseu e-mail");
+let email= prompt("digite seu email");
+alert("seu email é " + email);
+
+console.log(nome);
+console.log(num);
+console.log(num2);
+console.log(seuNome);
+console.log(idade);
+console.log(email);
+
